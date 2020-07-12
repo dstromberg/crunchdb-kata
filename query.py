daff4064088081d901a021366f8ee8ff671c16b6
Dan Stromberg <strombrg@gmail.com>
2020-07-12 09:10:42 -0700

    obtain a set of keys and print

--- query.py
+++ query.py
@@ -6,24 +6,28 @@ import gzip
 import json
 import os
 import pathlib
+import pprint
+
+import unidecode
 
 
 def ascii_encode_dict(data):
     """
     Insist on ASCII data, so we can print to the console during debugging.
 
+    Assumes 'data' isn't nested at all.
+
     Based on https://stackoverflow.com/questions/9590382/forcing-python-json-module-to-work-with-ascii
     """
     # This is converting the data to bytestrings at the moment.  But it's close to what I want, so I'm committing it anyway.
-    def ascii_encode(x):
-        print('barney 1', type(x))
-        result = x.encode('ascii', 'replace')
-        print('barney 2', type(result))
+    def ascii_encode(string):
+        assert isinstance(string, str)
+        result = unidecode.unidecode(string)
         return result
     return {ascii_encode(key): ascii_encode(value) for key, value in data.items()}
 
 
-def get_json_documents():
+def get_json_documents(convert=True):
     """Return new JSON documents, at least 50 at a time."""
     # We assume a single consumer process.
     path = pathlib.Path('json-data')
@@ -32,21 +36,23 @@ def get_json_documents():
         for json_filename in json_filenames:
             with gzip.open(json_filename, "r") as answerfile:
                 bytes_data = answerfile.read()
-            # print('fred 1', bytes_data)
             os.unlink(json_filename)
-            # convert to ascii and yield
             str_data = bytes_data.decode('UTF-8')
-            # print('fred 2', str_data)
-            json_dict = json.loads(str_data, object_hook=ascii_encode_dict)
-            print('fred 3', json_dict)
+            if convert:
+                # This is nice for debugging
+                json_dict = json.loads(str_data, object_hook=ascii_encode_dict)
+            else:
+                json_dict = json.loads(str_data)
             yield json_dict
 
 
 def main():
     """Get the ball rolling."""
-    for _json_document in get_json_documents():
-        pass
-        # print(json_document)
+    keys = set()
+    for json_document in get_json_documents():
+        for key in json_document:
+            keys.add(key)
+    pprint.pprint(keys)
 
 
 if __name__ == '__main__':
